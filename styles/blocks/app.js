523a1fb5aa8fe279701a5cdda9b170bf087f5471
Alexandr Novosyolov <aain34@mail.ru>
2024-03-02 18:35:36 +0300

    добавлена разметка под секцию construction и request-overlay

--- styles/blocks/app.js
+++ styles/blocks/app.js
@@ -4,3 +4,11 @@ const burgerButton = document.querySelector("#burgerButton");
 burgerButton.addEventListener("click", () => {
   dialogElem.showModal();
 });
+
+const requestOverlay = document.querySelector('#requestOverlay')
+const submitRequestButton = document.querySelector('#submitRequest')
+
+submitRequestButton.addEventListener('click', ()=>{
+  requestOverlay.showModal()
+}
+  )
