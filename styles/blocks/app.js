51a0a92b9e8368b4ce295680bac192c81f10defb
Alexandr Novosyolov <aain34@mail.ru>
2024-03-07 21:35:06 +0300

    Изменен mobile-overlay

--- styles/blocks/app.js
+++ styles/blocks/app.js
@@ -1,14 +1,18 @@
-const dialogElem = document.querySelector("#mobileOverlay");
+const mobileOverlay = document.querySelector("#mobileOverlay");
 const burgerButton = document.querySelector("#burgerButton");
+const mobileOverlayCloseButton = document.querySelector(
+  ".mobile-overlay__close-button"
+);
 
 burgerButton.addEventListener("click", () => {
-  dialogElem.showModal();
+  mobileOverlay.classList.toggle("mobile-overlay-open");
 });
 
-const requestOverlay = document.querySelector('#requestOverlay')
-const submitRequestButton = document.querySelector('#submitRequest')
-
-submitRequestButton.addEventListener('click', ()=>{
-  requestOverlay.showModal()
+function closeModal(event) {
+  const target = event.target;
+  target === mobileOverlayCloseButton || target === mobileOverlay
+    ? mobileOverlay.classList.remove("mobile-overlay-open")
+    : "";
 }
-  )
+
+mobileOverlay.addEventListener("click", closeModal);
