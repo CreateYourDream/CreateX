9acf1f62062c6685e2c31129a8804462455f2155
Alexandr Novosyolov <aain34@mail.ru>
2024-03-10 20:58:17 +0300

    закончен request-overlay, исправлен mobile-overlay

--- styles/blocks/app.js
+++ styles/blocks/app.js
@@ -1,3 +1,4 @@
+
 const mobileOverlay = document.querySelector("#mobileOverlay");
 const burgerButton = document.querySelector("#burgerButton");
 const mobileOverlayCloseButton = document.querySelector(
@@ -10,9 +11,26 @@ burgerButton.addEventListener("click", () => {
 
 function closeModal(event) {
   const target = event.target;
-  target === mobileOverlayCloseButton || target === mobileOverlay
+  target === mobileOverlayCloseButton || mobileOverlay
     ? mobileOverlay.classList.remove("mobile-overlay-open")
     : "";
 }
 
 mobileOverlay.addEventListener("click", closeModal);
+
+///////requestOverlay/////////////
+
+const requestOverlay = document.querySelector('.request-overlay')
+const requestOverlayOpenButton = document.querySelector('.construction__body-actions-button')
+
+requestOverlayOpenButton.addEventListener("click", () => {
+  requestOverlay.classList.toggle("request-overlay-open");
+});
+
+function closeRequestOverlay(event) {
+  const target = event.target;
+  target === requestOverlay
+    ? requestOverlay.classList.remove("request-overlay-open")
+    : "";
+}
+requestOverlay.addEventListener("click", closeRequestOverlay);
